82204078e94af2bbb2e513f1d8aa1eff2ac38e53
Aahad Yamani <aahadyamani09@gmail.com>
2022-10-03 14:39:08 -0700

    function that creates an array of chars and initialises it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,10 @@ if (size == 0)
 	return (NULL);
 cr = malloc(sizeof(c) * size);
 
-if (cr = NULL)
+if ([cr] = NULL)
 	return (NULL);
 
-for (i = 0; index < size; i++)
+for (i = 0; i < size; i++)
 	cr[i] = c;
 return (cr);
 }

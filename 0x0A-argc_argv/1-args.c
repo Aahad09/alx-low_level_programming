74f7aeed114fe512b466ccee733fcf4bca67c656
Aahad Yamani <aahadyamani09@gmail.com>
2022-10-04 14:25:32 -0700

    progra that prints the number of arguements passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the nuber of arguements
+ * @argc: number of arguements
+ * @argv: array of arguements
+ * Return: the number of arguements
+ */
+
+int main(int argc, char __atribute__((unused)) *argv[])
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}

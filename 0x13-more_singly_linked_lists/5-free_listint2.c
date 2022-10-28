ff9886021c7538ab06c7e7d849e153f7def2fe99
Aahad Yamani <aahadyamani09@gmail.com>
2022-10-28 10:48:44 -0700

    function that frees a linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,7 +5,7 @@
  * @head: pointer to the head of the list
  * Return: void
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 listint_t *cursor;
 listint_t **temp = head;
